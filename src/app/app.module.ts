f9bebe32c1b0aff1a724df70dc252e74f66dda04
2koders <2koders@gmail.com>
2020-09-19 18:22:25 -0400

    Componentes principales y paginas creadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingComponent,
+    ContactsComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
