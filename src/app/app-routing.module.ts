6cbce96378652937a47563e05ce100a48f746051
2koders <2koders@gmail.com>
2020-09-20 01:32:11 -0400

    LandingPage listo!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ErrorNotFoundComponent } from './pages/error-not-found/error-not-found.component';
 
 const routes: Routes = [
   {
     path: '', component: LandingComponent
+  },
+  {
+    path: 'contacts', component: ContactsComponent
+  },
+  {
+    path: 'about', component: AboutUsComponent
+  },
+  {
+    path: '404error', component: ErrorNotFoundComponent
+  },
+  {
+    path: '', redirectTo: '/', pathMatch: 'full'
+  },
+  {
+    path: '**', redirectTo: '404error'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ErrorNotFoundComponent } from './pages/error-not-found/error-not-found.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { AboutUsComponent } from './pages/about-us/about-us.component';
     FooterComponent,
     LandingComponent,
     ContactsComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    ErrorNotFoundComponent,
+    SliderComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
